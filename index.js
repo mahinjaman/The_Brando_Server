c798af3bef8aa68d10b5d0fd7c6a71e6bcd5ab22
unknown <mahinjaman07>
2024-07-26 11:01:50 +0600

    create update status booking and room

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
-
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 // middleware
 
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:5173', 'http://localhost:5174']
+}))
 app.use(express.json())
+app.use(cookieParser())
+
+// My Middleware
+
+const verifyUser = (req, res, next) => {
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'UnAuthorized' })
+    }
+
+    jwt.verify(token, process.env.SECURITY_JWT_TOKEN, (error, decode) => {
+        if (error) return res.status(403).json({ message: 'Access denied. Invalid token.' });
+        req.user = decode;
+        next();
+    })
+}
 
 // routes
 
@@ -20,6 +40,7 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const e = require('express');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oqwbmox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,22 +52,42 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
 
     const roomsCollections = client.db('TheBrando').collection('rooms');
     const roomsFacilityCollections = client.db('TheBrando').collection('room_facility');
     const latestNewsCollections = client.db('TheBrando').collection('news');
     const testimonialsCollections = client.db('TheBrando').collection('testimonial');
-
+    const restaurantCollections = client.db('TheBrando').collection('restaurant_menu');
+    const bookingCollections = client.db('TheBrando').collection('booking');
 
     try {
+
+
+        // JWT Verify
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECURITY_JWT_TOKEN, {
+                expiresIn: '1hr'
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none'
+                })
+                .send({ success: true });
+        })
+
 
         // get popular rooms
 
         app.get('/popular_rooms', async (req, res) => {
             const popularRooms = await roomsCollections.find()
-            .limit(5)
-            .toArray();
+                .limit(5)
+                .toArray();
             res.send(popularRooms)
         })
 
@@ -56,7 +97,6 @@ async function run() {
             const page = parseInt(req.query.page);
             const limit = parseInt(req.query.limit);
             const skip = (page - 1) * limit;
-            console.log(skip);
             const rooms = await roomsCollections.find()
                 .skip(skip)
                 .limit(limit)
@@ -85,7 +125,11 @@ async function run() {
 
 
         // get specific room
-        app.get('/room/:id', async (req, res) => {
+        app.get('/room/:id', verifyUser, async (req, res) => {
+            // if(req.user != process.env.SECURITY_JWT_TOKEN){
+            //     return res.status(403).send({ message: 'Access denied. Invalid token.' });
+            // }
+            console.log(req.user);
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await roomsCollections.findOne(query)
@@ -95,16 +139,107 @@ async function run() {
         // get total_room count from mongodb
         app.get('/total_rooms', async (req, res) => {
             const count = await roomsCollections.countDocuments();
-            console.log(count);
             res.send({ count })
         })
 
+        // get restaurant menu
+        app.get('/restaurant_menu', async (req, res) => {
+            const restaurantMenu = await restaurantCollections.find().toArray()
+            res.send(restaurantMenu)
+        })
+
+        // get search room 
+
+
+
+        // Clear cookie
+        app.post('/log_out', async (req, res) => {
+            res.clearCookie('token');
+            res.send({ success: true });
+        })
+
+        // room booking request
+
+        app.post('/booking', async (req, res) => {
+            const room = await req.body;
+            const filter = { _id: new ObjectId(room.room_id) };
+            const options = { upsert: true };
+            const updateStatus = {
+                $set: { status: 'booked' }
+            }
+            const updateRoom = await roomsCollections.updateOne(filter, updateStatus, options);
+            const result = await bookingCollections.insertOne(room);
+            res.send(result);
+        })
+
+        // get booking rooms for the current user
+
+        app.get('/booking', verifyUser, async (req, res) => {
+            const currentUser = req.user;
+            if (currentUser?.email != req.query.email) {
+                res.status(403).send({ message: 'Access denied. Invalid token.' });
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query?.email }
+            }
+
+            const result = await bookingCollections.find(query).toArray()
+            res.send(result)
+
+        })
+
+        // update booking status confirmed
+
+        app.put('/booking_confirmed/:id', async (req, res) => {
+            const currentUser = req.body;
+            const id = req.params.id;
+            const orderStatus = req.query?.status;
+            const query = { _id: new ObjectId(id) }
+            const updateStatus = {
+                $set: { orderStatus: 'Confirmed' }
+            }
+            const updateResult = await bookingCollections.updateOne(query, updateStatus);
+            res.send(updateResult)
+        })
+
+        // update booking room status cancelled
+        app.put('/booking_cancelled/:id', verifyUser, async (req, res) => {
+            const currentUser = req.body;
+            const id = req.params.id;
+            
+            if (currentUser?.email != req.query?.email) {
+                res.status(403).send({ message: 'Access denied. Invalid token.' });
+            }
+
+            const orderStatus = req.query?.status;
+            const filter = { _id: new ObjectId(id) };
+            const updateStatus = {
+                $set: { orderStatus }
+            }
+
+            const updateResult = await bookingCollections.updateOne(filter, updateStatus);
+            res.send(updateResult);
+
+
+        });
+
+        // Room Status Update Available
+
+        app.patch('/room_available/:id', async (req, res) => {
+            const id = req.params?.id;
+            const roomFilter = { _id: new ObjectId(id) };
+            const roomUpdateStatus = {
+                $set: { status: 'Available' }
+            }
+            const roomUpdateResult = await roomsCollections.updateOne(roomFilter, roomUpdateStatus);
+            return res.send(roomUpdateResult)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        console.log('connection Done');
     }
 }
 run().catch(console.dir);
