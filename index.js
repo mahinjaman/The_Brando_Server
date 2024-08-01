72d71494298d167f8d0127941be01d3dc8bbd62e
unknown <mahinjaman07>
2024-08-02 01:24:20 +0600

    get all booking method create

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const cookieParser = require('cookie-parser')
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:5174']
-    // origin: ['https://the-brando.web.app', 'https://console.firebase.google.com/project/the-brando/overview']
+    // origin: ['http://localhost:5173', 'http://localhost:5174']
+    origin: ['https://the-brando.web.app', 'https://console.firebase.google.com/project/the-brando/overview']
 }))
 app.use(express.json())
 app.use(cookieParser())
