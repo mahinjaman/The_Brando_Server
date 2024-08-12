2c6c632cc30eb718e3b17874be259eb522947f1e
unknown <mahinjaman07>
2024-08-13 01:22:21 +0600

    create user post method

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cookieParser = require('cookie-parser')
 
 app.use(cors({
     credentials: true,
-    origin: ['https://the-brando.web.app', 'https://console.firebase.google.com/project/the-brando/overview','http://localhost:5173', 'http://localhost:5174']
+    origin: ['http://localhost:5173', 'http://localhost:5174']
+    // origin: ['https://the-brando.web.app', 'https://console.firebase.google.com/project/the-brando/overview']
 }))
 app.use(express.json())
 app.use(cookieParser())
@@ -61,6 +62,7 @@ async function run() {
     const testimonialsCollections = client.db('TheBrando').collection('testimonial');
     const restaurantCollections = client.db('TheBrando').collection('restaurant_menu');
     const bookingCollections = client.db('TheBrando').collection('booking');
+    const userCollections = client.db('TheBrando').collection('users');
 
     try {
 
@@ -185,15 +187,17 @@ async function run() {
 
         // update booking status confirmed
 
-        app.put('/bookingConfirmed/:id', async (req, res) => {
-            const id = req.params.id;
+        app.patch('/bookingStatus/:id', async (req, res) => {
+            const id = req.params.id;            
             const query = { _id: new ObjectId(id) }
+            const orderStatus = req.query?.status;
             const updateStatus = {
-                $set: { orderStatus: 'Confirmed' }
+                $set: { orderStatus }
             }
             const updateResult = await bookingCollections.updateOne(query, updateStatus);
             res.send(updateResult)
         })
+
 
         // update booking room status cancelled
         app.put('/booking_cancelled/:id', verifyUser, async (req, res) => {
@@ -222,6 +226,7 @@ async function run() {
             const id = req.params?.id;
             const roomFilter = { _id: new ObjectId(id) };
             const status = req.query.status;
+            
             const roomUpdateStatus = {
                 $set: { status }
             }
@@ -233,6 +238,20 @@ async function run() {
         app.get('/all_bookings', async (req, res) => {
             const result = await bookingCollections.find().toArray()
             res.send(result)
+        })
+
+        // Users Collections
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const email = req.body.email;
+            const query = { email };
+            const userExist = await userCollections.findOne(query);
+            if (userExist) {
+                return res.status(400).send({ message: 'User already exists' });
+            }
+            const result = await userCollections.insertOne(user);
+            res.send(result);
         })
 
         // await client.db("admin").command({ ping: 1 });
